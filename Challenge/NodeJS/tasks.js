51669f5e71abdef420cc1b737095947ffc223313
wanasx <wanasx@gmail.com>
2020-01-20 11:57:44 +0200

    help command

--- Challenge/NodeJS/tasks.js
+++ Challenge/NodeJS/tasks.js
@@ -36,6 +36,8 @@ function onDataReceived(text) {
     quit();
   } else if (text === "hello\n") {
     hello();
+  } else if (text === "help\n") {
+    help();
   } else {
     unknownCommand(text);
   }
@@ -69,6 +71,12 @@ function hello() {
 function quit() {
   console.log("Quitting now, goodbye!");
   process.exit();
+}
+/**
+ * help
+ */
+function help() {
+  console.log("do you mean quit \n exit \n hello  ");
 }
 
 // The following line starts the application
