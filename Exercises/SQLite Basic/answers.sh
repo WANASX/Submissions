d4c4c742b394f2d54f2f6183553d1e30de6cc05d
wanasx <wanasx@gmail.com>
2020-01-20 11:22:57 +0200

    Joins

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -16,12 +16,14 @@ VALUES (8, anas, Age, Gender,Points);
 7 UPDATE students SET Points = 200 WHERE Name='Alex'; 
 
 
-
-
 1 - CREATE TABLE "graduates" ( `ID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `Name` TEXT NOT NULL UNIQUE, `Age` TEXT, `Gender` TEXT, `Points` INTEGER )
-
 
 2 - INSERT INTO graduates (Name, Age, Gender,Points) SELECT Name, Age, Gender, Points FROM students where Name='Layal';
 
-
 3 - delete from students where Name='Layal'
+
+
+1 - CREATE TABLE "ProduceTable" ( `ID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `Name` TEXT NOT NULL UNIQUE, `Company` TEXT,`CompanyDate` date )
+
+   1.1 - insert into ProduceTable (Name, Company, CompanyDate) select  e.Name, e.Company,c.Date 
+          from employees e,companies c where e.Company =c.Name
