cec4eab25f0467c88d065deee4926821dde04175
wanasx <wanasx@gmail.com>
2020-01-23 10:29:22 +0200

    step 6

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -8,6 +8,51 @@ const movies = [
   { title: "Brazil", year: 1985, rating: 8 },
   { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 }
 ];
+/* ORDERED BY DATE */
+
+function byDate(a, b) {
+  const bandA = a.year;
+  const bandB = b.year;
+
+  let byDate = 0;
+  if (bandA > bandB) {
+    byDate = 1;
+  } else if (bandA < bandB) {
+    byDate = -1;
+  }
+  return byDate;
+}
+var lastarray = movies.sort(byDate);
+
+/* by rating */
+function byRate(a, b) {
+  const bandA = a.rating;
+  const bandB = b.rating;
+
+  let byRate = 0;
+  if (bandA < bandB) {
+    byRate = 1;
+  } else if (bandA > bandB) {
+    byRate = -1;
+  }
+  return byRate;
+}
+var lastbyRate = movies.sort(byRate);
+/* by rating */
+function byTitle(a, b) {
+  const bandA = a.title;
+  const bandB = b.title;
+
+  let byTitle = 0;
+  if (bandA < bandB) {
+    byTitle = 1;
+  } else if (bandA > bandB) {
+    byTitle = -1;
+  }
+  return byTitle;
+}
+var lastbyTitle = movies.sort(byTitle);
+console.log(lastbyTitle);
 
 var port = 3000;
 var app = express();
@@ -49,10 +94,19 @@ app.get("/movies/read", (req, res) => {
   res.send({ status: 200, data: movies });
 });
 app.get("/movies/update", (req, res) => {
-  res.send({ status: 200, message: TIME() });
+  res.send();
 });
 app.get("/movies/delete", (req, res) => {
-  res.send({ status: 200, message: TIME() });
+  res.send();
+});
+app.get("/movies/read/by-date", (req, res) => {
+  res.send({ status: 200, data: lastarray });
+});
+app.get("/movies/read/by-rating", (req, res) => {
+  res.send({ status: 200, data: lastbyRate });
+});
+app.get("/movies/read/by-title", (req, res) => {
+  res.send({ status: 200, data: lastbyTitle });
 });
 var server = http.createServer(app);
 server.listen(port, () => {
